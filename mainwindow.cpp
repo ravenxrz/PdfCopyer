8a5dc3114830e3f0414dcb649d5615cd5c0ae8ae
ravenxrz <zhang.xingrui@foxmail.com>
2021-04-26 15:49:05 +0800

    支持自定义快捷键

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,11 +2,17 @@
 #include <QDialog>
 #include <QMenu>
 #include <QCoreApplication>
+#include <QFile>
 #include "mainwindow.h"
 #include "clipboardprocessor.h"
 
+// init static vars
+const QString MainWindow::kNormalModeKey = "去除末尾换行符";
+const QString MainWindow::kCodeModeKey = "去除行号";
+
+
 MainWindow::MainWindow(QWidget *parent)
-    : QWidget(parent)
+    : QWidget(parent), hotKeyFilePath("./自定义快捷键.txt")
 {
     // 初始化action和trayicon
     createActions();
@@ -32,11 +38,13 @@ void MainWindow::setVisible(bool visible)
 
 void MainWindow::normalModeProcess()
 {
+    qDebug() << "Normal Mode Process";
     ClipboardProcessor::normalModeProcess();
 }
 
 void MainWindow::codeModeProcess()
 {
+    qDebug() << "Code Mode Process";
     ClipboardProcessor::codeModeProcess();
 }
 
@@ -59,12 +67,86 @@ void MainWindow::createTrayIcon()
     trayIcon->setIcon(QIcon(":/images/heart.png"));
 }
 
-void MainWindow::crateHotKey()
-{
-    // 文本处理快捷键
-    normalModeHotKey = new QHotkey(QKeySequence("alt+1"), true, this);//The hotkey will be automatically registered
-    // 代码处理快捷键
-    codeModeHotKey = new QHotkey(QKeySequence("alt+2"),true,this);
+/**
+ * 向file所在文件中，写入 "key=value" 键值对
+ * @brief writeKeyValue
+ * @param file
+ * @param key
+ * @param value
+ */
+static void writeKeyValue(QFile *file,const QString &key,const QString &value){
+    Q_ASSERT(file->isOpen() && file->isWritable());
+    QString line = key + "=" + value  + "\n";
 
+    const auto &bytes = line.toUtf8();
+    file->write(bytes.data(),bytes.size());
 }
 
+/**
+ * @brief readKeyFromFile
+ * @param file
+ * @param key
+ * @param value
+ * @return
+ */
+static bool setHotkeyMapFromFile(const QString &filePath, QMap<QString, QString> *map){
+    Q_ASSERT(QFile::exists(filePath));
+    char buf[1024];
+    bool ret = false;
+    QFile fileObj(filePath);
+    ret = fileObj.open(QIODevice::ReadOnly);
+    Q_ASSERT(ret);
+
+    while(fileObj.readLine(buf,sizeof(buf)) != -1){
+        QString tmp(buf);
+        QString line = tmp.trimmed();
+        if(line.startsWith(MainWindow::kNormalModeKey)){
+            (*map)[MainWindow::kNormalModeKey] = line.mid(MainWindow::kNormalModeKey.length()+1);
+        }else if(line.startsWith(MainWindow::kCodeModeKey)){
+            (*map)[MainWindow::kCodeModeKey] = line.mid(MainWindow::kCodeModeKey.length()+1);
+        }else{
+            qDebug() << "Parse custom shortcut file failed\n";
+            exit(-1);
+        }
+    }
+    return true;
+}
+
+void MainWindow::initHotKeyMap()
+{
+    Q_ASSERT(!hotKeyFilePath.isEmpty());
+    QFile file(hotKeyFilePath);
+    bool ret = false;
+    QString keyForNormalMode ="alt+1";
+    QString keyForCodeMode = "alt+2";
+
+    qDebug() << file.exists();
+    if (!file.exists()){
+        ret = file.open(QIODevice::WriteOnly | QIODevice::Text);
+        Q_ASSERT(ret);
+        // write default hot key
+        writeKeyValue(&file,kNormalModeKey,keyForNormalMode);
+        writeKeyValue(&file,kCodeModeKey,keyForCodeMode);
+        file.close();
+    }
+
+    ret = setHotkeyMapFromFile(hotKeyFilePath,&hotKeyMap);
+    Q_ASSERT(ret);
+}
+
+
+void MainWindow::crateHotKey()
+{
+    initHotKeyMap();
+
+    QString keyForNormalMode = hotKeyMap[MainWindow::kNormalModeKey];
+    QString keyForCodeMode = hotKeyMap[MainWindow::kCodeModeKey];
+
+    qDebug() << "Normal mode key:" << keyForNormalMode;
+    qDebug() << "Code mode key:" << keyForCodeMode;
+
+    // 文本处理快捷键
+    normalModeHotKey = new QHotkey(QKeySequence(keyForNormalMode), true, this);//The hotkey will be automatically registered
+    // 代码处理快捷键
+    codeModeHotKey = new QHotkey(QKeySequence(keyForCodeMode),true,this);
+}
--- mainwindow.h
+++ mainwindow.h
@@ -27,13 +27,28 @@ private:
     void createTrayIcon();
     void crateHotKey();
 
+    // 初始化热键
+    // 从默认hotKeyFilePath中读取键位映射
+    // 若hotKeyFilePath文件不存在，则创建默认文件
+    // 默认规则：
+    // Alt+1 --> Normal Mode
+    // Alt+2 --> Code Mode
+    void initHotKeyMap();
+
 private:
     QAction *quitAction;
 
-    QHotkey *normalModeHotKey;      // 文本处理快捷键
-    QHotkey *codeModeHotKey;        // 代码处理快捷键
+
+    QHotkey *normalModeHotKey;                  // 文本处理快捷键
+    QHotkey *codeModeHotKey;                    // 代码处理快捷键
+    QMap<QString,QString> hotKeyMap;            // hot key映射
+    QString hotKeyFilePath;                     // 自定义hot key文件路径
 
     QSystemTrayIcon *trayIcon;
+
+public:
+    static const QString kNormalModeKey;
+    static const QString kCodeModeKey;
 
 };
 #endif // MAINWINDOW_H
